efa93b6fd5ff398447cbd1ff8210021d6ce39e83
Raquel Guzansky <raquelguzansky@MacBook-Air-de-Raquel.local>
2023-12-08 14:55:35 -0300

    WebConfig adicionado para tratar Cors

---
+++ src/main/java/br/com/vaidaruim/gs3/config/WebConfig.java
@@ -0,0 +1,21 @@
+package br.com.vaidaruim.gs3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://snack-web-player.s3.us-west-1.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
