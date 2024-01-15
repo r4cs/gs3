4db9fc30370e3b6ee7916fabe92aea5b50757dc2
Raquel Guzansky <raquelguzansky@MacBook-Air-de-Raquel.local>
2024-01-15 15:37:12 -0300

    alterando url de setUrl na classe SwaggerConfig 4

--- src/main/java/br/com/vaidaruim/gs3/config/SwaggerConfig.java
+++ src/main/java/br/com/vaidaruim/gs3/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ public class SwaggerConfig {
     public OpenAPI geraDocumentacao() {
         Server devServer = new Server();
         devServer.setUrl("https://medicamixbackendwebapp.azurewebsites.net/");
+        devServer.setUrl("https://medicamixbackendwebapp.azurewebsites.net/api/farmacos");
         devServer.setUrl("https://medicamixbackendwebapp.azurewebsites.net/swagger-ui/index.html");
 //        devServer.setUrl("http://localhost:8080/api/farmacos"); ---> localmente, está setado assim, esse link específico retorna whitelabel (desconsiderar esse erro)
         devServer.description("Url de desenvolvimento azure");
